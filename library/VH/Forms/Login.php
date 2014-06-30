85e14cabc39881e26d0e9923a8a64063d6b43924
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-06-30 11:44:09 -0300

    Criado formulario de Login

---
+++ library/VH/Forms/Login.php
@@ -0,0 +1,28 @@
+<?php
+
+class VH_Forms_Login extends Zend_Form {
+    
+    public function _init() {
+        
+        # Método POST
+        $this->setMethod("POST");
+        
+        # Campo Login
+        $login = $this->createElement("text", "login", array("label" => "Login:", "class" => "input-g"))
+                ->setRequired(true);
+        $this->addElement($login);
+        
+        # Campo Senha
+        $senha = $this->createElement("password", "senha", array("label" => "Senha:", "class" => "input-g"))
+                ->setRequired(true);
+        $this->addElement($senha);
+        
+        # Botão Acessar
+        $submit = $this->createElement("submit", "submit", array("label" => "Acessar", "class" => "input-p"));
+        $this->addElement($submit);
+        
+        # Segurança
+        $this->addElement("hash", "csrf");
+    }
+}
+
