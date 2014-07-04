7d171bb14a048550caa941ea15f6085c4cd2125b
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-07-04 01:33:18 -0300

    Criado formulario de Tipos

---
+++ library/VH/Forms/Tipos.php
@@ -0,0 +1,24 @@
+<?php
+
+class VH_Forms_Tipos extends Zend_Form
+{
+    public function init() {
+        
+        # Método POST
+        $this->setMethod("POST");
+        
+        # Campo Nome
+        $nome = $this->createElement("text", "Nome", array("label" => "Nome:", "class" => "input-m"))
+                ->setRequired(true);
+        $this->addElement($nome);
+        
+        # Campo Descrição
+        $descricao = $this->createElement("text", "Descricao", array("label" => "Descrição:", "class" => "input-g"));
+        $this->addElement($descricao);
+        
+        # Botão Salvar
+        $submit = $this->createElement("submit", "submit", array("label" => "Salvar", "class" => "input-p"));
+        $this->addElement($submit);
+    }
+}
+
