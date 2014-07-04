09f9f06f1e355eb02a0bc7a0223d0a2955d08908
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-07-04 01:15:56 -0300

    Implementado Modelo TipoMapper

--- application/models/TipoMapper.php
+++ application/models/TipoMapper.php
@@ -1,8 +1,36 @@
 <?php
 
-class Application_Model_TipoMapper
+class TipoMapper extends VH_Db_DataMapperAbstract
 {
+    protected $_dbTable = "DbTable_Tipo";
+    protected $_model = "Tipo";
+    
+    protected function _insert(\VH_Db_DomainObjectAbstract $obj) {
+        try {
+            $dbTable = $this->getDbTable();
+            $data = array(
+                "Nome" => $obj->getNome(),
+                "Descricao" => $obj->getDescricao()
+            );
+            $dbTable->insert($data);
+            return true;
+        } catch (Zend_Exception $e) {
+            return false;
+        }
+    }
 
-
+    protected function _update(\VH_Db_DomainObjectAbstract $obj) {
+        try {
+            $dbTable = $this->getDbTable();
+            $data = array(
+                "Nome" => $obj->getNome(),
+                "Descricao" => $obj->getDescricao()
+            );
+            $dbTable->update($data, array("id = ?" => $obj->getId()));
+            return true;
+        } catch (Zend_Exception $e) {
+            return false;
+        }
+    }
 }
 
