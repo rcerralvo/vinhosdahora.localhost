76942b0b1ab5be7994881faa3b61498def0b0745
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-07-05 22:26:50 -0300

    Adicionado ZendX

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,6 +9,8 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->doctype("HTML5");
         Zend_Registry::set('view', $view);
         
+        $view->addHelperPath("ZendX/JQuery/View/Helper","ZendX_JQuery_View_Helper");
+        
         $currency = new Zend_Currency("pt_BR");
         $currency->setFormat(array('symbol' => "R$ "));
         Zend_Registry::set("currency", $currency);
@@ -32,7 +34,5 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         
         $front->registerPlugin(new VH_Plugins_Layout());
     }
-
-
 }
 
