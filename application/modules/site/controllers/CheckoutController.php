ed2e642f8c54e65231577033aa975afd181ce63b
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-07-26 00:44:19 -0300

    Implementação do método indexAction

--- application/modules/site/controllers/CheckoutController.php
+++ application/modules/site/controllers/CheckoutController.php
@@ -11,6 +11,24 @@ class Site_CheckoutController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $carrinho = Carrinho::getInstance();
+        Zend_Session::regenerateId();
+        
+        if (!$carrinho->isEmpty()) {
+            $formLogin = new VH_Forms_Login();
+            $formLogin->setAction(Zend_Controller_Front::getInstance()->getBaseUrl() . "/auth");
+            $this->view->formLogin = $formLogin;
+            
+            $formCliente = new VH_Forms_ClientesCadastro();
+            $formCliente->setAction(Zend_Controller_Front::getInstance()->getBaseUrl() . "/clientes");
+            $this->view->formCliente = $formCliente;
+            
+            $this->view->carrinho = $carrinho->fetchAll();
+            $this->view->total = $carrinho->getTotal();
+        }
+        else {
+            $this->_redirect("carrinho");
+        }
     }
 
 
