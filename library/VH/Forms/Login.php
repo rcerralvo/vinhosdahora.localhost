cb4ab18ca54f2bef6ed23250eab27e0000c5d8a5
Rafael Aparecido Cerralvo <rafaelcerralvo@gmail.com>
2014-07-26 16:45:33 -0300

    adicionando validações

--- library/VH/Forms/Login.php
+++ library/VH/Forms/Login.php
@@ -9,12 +9,20 @@ class VH_Forms_Login extends Zend_Form {
         
         # Campo Login
         $login = $this->createElement("text", "login", array("label" => "Login:", "class" => "input-g"))
-                ->setRequired(true);
+                ->setRequired(true)
+                ->addValidator('NotEmpty')
+                ->addFilter('StripTags')
+                ->addFilter('StringTrim')
+                ->addErrorMessage('Informe o seu Login');
         $this->addElement($login);
         
         # Campo Senha
         $senha = $this->createElement("password", "senha", array("label" => "Senha:", "class" => "input-g"))
-                ->setRequired(true);
+                ->setRequired(true)
+                ->addValidator('NotEmpty')
+                ->addFilter('StripTags')
+                ->addFilter('StringTrim')
+                ->addErrorMessage('Informe a sua Senha');
         $this->addElement($senha);
         
         # Botão Acessar
